e5ec586be2187685e033f2b801ca01ed1d1ded26
zhoustkiy <mahammedalioro@gmai.com>
2024-03-11 10:36:48 +0600

    hello

---
+++ main.js
@@ -0,0 +1,67 @@
+//! = присваевание
+//! == сравнение 
+//! ===строгое сравнение
+//? !== не равно
+
+
+
+
+
+
+//!ex1
+// let sum = 0;
+
+
+// for(let i =1; i <= 10; i++) {
+//     sum =+ i;
+// }
+
+// console.log(sum);
+
+//!ex2
+// for (let i = 1; i <= 10; i++) {
+//     console.log(5 * i);
+// }
+
+
+
+//!ex3
+// for (let i =2; i <= 20; i +=2){
+//     console.log(i);
+// }
+
+
+//!ex4
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+
+//!ex5 
+// let factorial = prompt()
+// let sum =1
+
+// for(let i = 1; i <=factorial; i++) {
+//     sum *= i;
+// }
+// console.log(sum);
+
+
+//!ex6
+
+
+
+
+
+
+
+
+
+
+
+//!ex7
+let sum = 0;
+for (let i = 0; i <= 10; i += 2) {
+sun += i;
+}
+console.log(sum);
